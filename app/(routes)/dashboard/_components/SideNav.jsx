3a27bb9de793c29457f92e1fee13cc5db2dbf7a3
Bijay Kumar shrestha <143540123+BijayX@users.noreply.github.com>
2024-09-07 22:34:30 +0545

    Update SideNav.jsx

--- app/(routes)/dashboard/_components/SideNav.jsx
+++ app/(routes)/dashboard/_components/SideNav.jsx
@@ -14,6 +14,7 @@ import {
 import { UserButton } from "@clerk/nextjs";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
+
 function SideNav() {
   const menuList = [
     {
@@ -70,36 +71,39 @@ function SideNav() {
   useEffect(() => {
     console.log(path);
   }, [path]);
+
   return (
-    <div className="h-screen p-5 border shadow-sm">
-      <div className="flex flex-row items-center">
-      <Image src={"/chart-donut.svg"} alt="logo" width={40} height={25} />
-        <span className="text-blue-800 font-bold text-xl">FinSmart</span>
-      </div>
-      <div className="mt-5">
-        {menuList.map((menu, index) => (
-          <Link href={menu.path} key={index}>
-            <h2
-              className={`flex gap-2 items-center
-                    text-gray-500 font-medium
-                    mb-2
-                    p-4 cursor-pointer rounded-full
-                    hover:text-primary hover:bg-blue-100
-                    ${path == menu.path && "text-primary bg-blue-100"}
-                    `}
-            >
-              <menu.icon />
-              {menu.name}
-            </h2>
-          </Link>
-        ))}
+    <div className="h-screen p-5 border shadow-sm flex flex-col justify-between">
+      <div>
+        <div className="flex flex-row items-center">
+          <Image src={"/chart-donut.svg"} alt="logo" width={40} height={25} />
+          <span className="text-blue-800 font-bold text-xl">FinSmart</span>
+        </div>
+        <nav className="mt-5 overflow-y-auto flex-grow">
+          {menuList.map((menu, index) => (
+            <Link href={menu.path} key={index}>
+              <h2
+                className={`flex gap-2 items-center
+                      text-gray-500 font-medium
+                      mb-2
+                      p-4 cursor-pointer rounded-full
+                      hover:text-primary hover:bg-blue-100
+                      ${path == menu.path && "text-primary bg-blue-100"}
+                      `}
+              >
+                <menu.icon />
+                <span className="hidden sm:inline">{menu.name}</span>
+              </h2>
+            </Link>
+          ))}
+        </nav>
       </div>
       <div
-        className="fixed bottom-10 p-5 flex gap-2
-            items-center"
+        className="p-5 flex gap-2
+              items-center"
       >
         <UserButton />
-        Profile
+        <span className="hidden sm:inline">Profile</span>
       </div>
     </div>
   );
